018588fa7a9d99f59617ecc547ea35bbecc2f7cb
Pratiksha Tivale <pratikshativale05@gmail.com>
2026-03-28 16:31:17 +0530

    Update CharityList.jsx

--- src/components/CharityList.jsx
+++ src/components/CharityList.jsx
@@ -1,22 +1,38 @@
 import React from "react";
-import API from "../api/api";
 
 function CharityList({ charities, selectCharity }) {
   return (
-    <div className="card">
-      <h3>Charities</h3>
+    <div style={card}>
+      <h3>❤️ Charities</h3>
 
-      {charities.map((c) => (
-        <div className="item" key={c.id}>
-          {c.name}
-          <br />
-          <button onClick={() => selectCharity(c.id)}>
-            Select
-          </button>
-        </div>
-      ))}
+      {charities.length === 0 ? (
+        <p>No charities available</p>
+      ) : (
+        charities.map((c) => (
+          <div key={c.id} style={item}>
+            <p>{c.name}</p>
+
+            <button onClick={() => selectCharity(c.id)}>
+              Select
+            </button>
+          </div>
+        ))
+      )}
     </div>
   );
 }
 
 export default CharityList;
+
+const card = {
+  background: "#1e1e1e",
+  padding: "15px",
+  borderRadius: "8px",
+  marginBottom: "15px",
+  color: "white"
+};
+
+const item = {
+  borderBottom: "1px solid #333",
+  padding: "10px 0"
+};
